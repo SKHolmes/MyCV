ea16b8f0db54bb967e57d6915d6499256cf05660
SKHolmes <samualkholmes@gmail.com>
2016-03-21 02:33:01 +1300

    Functioning Skeleton Website

---
+++ app.js
@@ -0,0 +1,9 @@
+(function(document) {
+  'use strict';
+
+  var app = document.querySelector('#CV');
+  window.addEventListener('WebComponentsReady', function() {
+    alert('holy shit');
+  });
+  
+})(document);
