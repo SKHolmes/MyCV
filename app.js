a80878670a37fb26a70649a01f48dff54adb7b37
SKHolmes <samualkholmes@gmail.com>
2016-03-21 22:14:04 +1300

    Big commit. Added core functionality to CV. Collapseable bar works and is triggered by the button.

--- app.js
+++ app.js
@@ -3,16 +3,24 @@
 
   var app = document.querySelector('#app');
   window.addEventListener('WebComponentsReady', function() {
-    
 
+    var mainCollapse;
+    var toggled;
 
+    mainCollapse = document.createElement("iron-collapse");
+    Polymer.dom(mainCollapse).innerHTML = "Hi.";
+    document.getElementById("editable").appendChild(mainCollapse);
 
+    document.getElementById("contact-button").addEventListener("click", function(){
+      console.log("sup");
+      mainCollapse.toggle();
+      toggled = !toggled;      
+    });
 
+    app.displayContactDetails = function(){
+      console.log("this is okay");
+    }
 
-
-
-
-    
-  });
-  
+  });  
 })(document);
+
