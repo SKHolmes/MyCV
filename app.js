1935ba68e08a3443725db63fc13979233b4aba41
SKHolmes <samualkholmes@gmail.com>
2016-04-01 15:45:29 +1300

    Properly styled collapse how I want it to look and moved the iron-collapse from being dynamically generated to a part of the original HTML.

--- app.js
+++ app.js
@@ -6,15 +6,21 @@
 
     var mainCollapse;
     var toggled;
+    //var contactDetails = "<h1 margin='8px'>Contact Details.</h1><hr>";
+    var contactDetails = document.getElementById("contactDetails").innerHTML;
 
-    mainCollapse = document.createElement("iron-collapse");
-    Polymer.dom(mainCollapse).innerHTML = "Hi.";
-    document.getElementById("editable").appendChild(mainCollapse);
+/*    mainCollapse = document.createElement("iron-collapse");
+    Polymer.dom(mainCollapse).innerHTML = contactDetails;
+    Polymer.dom(mainCollapse).id = "collapse";
+    document.getElementById("editable").appendChild(mainCollapse);*/
+
+    mainCollapse = document.getElementById("collapse");
+    mainCollapse.innerHTML = contactDetails;
 
     document.getElementById("contact-button").addEventListener("click", function(){
-      console.log("sup");
       mainCollapse.toggle();
-      toggled = !toggled;      
+      toggled = !toggled;
+      app.displayContactDetails();      
     });
 
     app.displayContactDetails = function(){
