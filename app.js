c5c34d150c8e1a63adaacf9bc2b4955d1fd4ed04
SKHolmes <samualkholmes@gmail.com>
2016-04-08 04:19:38 +1200

    Added mroe contact Details. Added a work history drop and filled it in. Filled in references drop, Created and filled education drop.

--- app.js
+++ app.js
@@ -7,7 +7,9 @@
     app.DisplayEnums = {
       NONE: 'none',
       CONTACT: 'contact',
-      REFERENCES: 'references'
+      REFERENCES: 'references',
+      WORK_HISTORY: 'work-history',
+      EDUCATION: 'education'
     }
 
     var mainCollapse = document.getElementById('collapse');
@@ -20,16 +22,34 @@
     /*--------------Reference Details Collapseable--------------*/
     var referencesDetailsDiv = document.getElementById('referencesDetails');
 
+    /*--------------Work History Collapseable--------------*/
+    var workHistoryDetailsDiv = document.getElementById('workHistoryDetails');
+
+    /*--------------Work History Collapseable--------------*/
+    var educationDetailsDiv = document.getElementById('educationDetails');
+
     app.displayContactDetails = function(){
       var contactDetails = contactDetailsDiv.innerHTML;
       mainCollapse.innerHTML = contactDetails;
       currentDisplay = app.DisplayEnums.CONTACT;
     }
 
+    app.displayWorkHistoryDetails = function(){
+      var workHistoryDetails = workHistoryDetailsDiv.innerHTML;
+      mainCollapse.innerHTML = workHistoryDetails;
+      currentDisplay = app.DisplayEnums.WORK_HISTORY;
+    }
+
     app.displayReferenceDetails = function(){
       var referencesDetails = referencesDetailsDiv.innerHTML;
       mainCollapse.innerHTML = referencesDetails;
       currentDisplay = app.DisplayEnums.REFERENCES;
+    }
+
+    app.displayEducationDetails = function(){
+      var educationDetails = educationDetailsDiv.innerHTML;
+      mainCollapse.innerHTML = educationDetails;
+      currentDisplay = app.DisplayEnums.EDUCATION;
     }
 
     //Button Listeners
@@ -53,6 +73,26 @@
       }    
     });
 
+     document.getElementById('work-history-button').addEventListener('click', function(){
+      //If the collapse is open but displaying the details clicked, just close it.
+      if(mainCollapse.opened && currentDisplay == app.DisplayEnums.WORK_HISTORY){
+        mainCollapse.toggle();
+      //Else if the collapse is opened but displaying other details than that that was
+      //clicked. Close it, pause for the animation using a setTimeout function, change
+      //the details and reopen it.
+      }else if(mainCollapse.opened && currentDisplay != app.DisplayEnums.WORK_HISTORY){
+        mainCollapse.toggle();
+        setTimeout(function(){ 
+          app.displayWorkHistoryDetails();
+          mainCollapse.toggle();
+        }, animationDelay); 
+      //If the collapse is not opened we can just 
+      }else if(!mainCollapse.opened){
+        app.displayWorkHistoryDetails();
+        mainCollapse.toggle();
+      }
+    });
+
     document.getElementById('reference-button').addEventListener('click', function(){
       //If the collapse is open but displaying the details clicked, just close it.
       if(mainCollapse.opened && currentDisplay == app.DisplayEnums.REFERENCES){
@@ -69,6 +109,26 @@
       //If the collapse is not opened we can just 
       }else if(!mainCollapse.opened){
         app.displayReferenceDetails();
+        mainCollapse.toggle();
+      }
+    });
+
+    document.getElementById('education-button').addEventListener('click', function(){
+      //If the collapse is open but displaying the details clicked, just close it.
+      if(mainCollapse.opened && currentDisplay == app.DisplayEnums.EDUCATION){
+        mainCollapse.toggle();
+      //Else if the collapse is opened but displaying other details than that that was
+      //clicked. Close it, pause for the animation using a setTimeout function, change
+      //the details and reopen it.
+      }else if(mainCollapse.opened && currentDisplay != app.DisplayEnums.EDUCATION){
+        mainCollapse.toggle();
+        setTimeout(function(){ 
+          app.displayEducationDetails();
+          mainCollapse.toggle();
+        }, animationDelay); 
+      //If the collapse is not opened we can just 
+      }else if(!mainCollapse.opened){
+        app.displayEducationDetails();
         mainCollapse.toggle();
       }
     });
